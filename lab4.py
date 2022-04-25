eb05a8df9c4f1c78c7749b0ace6633dcc25ce3f2
achourali <achourali1999@gmail.com>
2022-04-26 00:20:42 +0100

    add DIANA based on kmeans

--- lab4.py
+++ lab4.py
@@ -17,7 +17,7 @@ def visualize():
     plt.show()
 
 
-def kmeans(n_clusters=4):       
+def kmeans(n_clusters=4):
     fromage = pd.read_table(r"./fromage1.txt", sep="\t", header=0, index_col=0)
     np.random.seed(0)
     kmeans = cluster.KMeans(n_clusters=n_clusters)
@@ -51,7 +51,7 @@ def agglomerative_hierarchical_clustering():
     plt.figure(figsize=(15, 8))
     plt.title("AHC")
     plt.title('AHC avec matérialisation des 4 classes')
-    dendrogram(Z, labels=fromage.index,
+    dendrogram(Z, labels=range(29),
                orientation='left', color_threshold=255)
     print(Z)
     plt.show()
@@ -62,12 +62,10 @@ def agglomerative_hierarchical_clustering():
         fromage.index[ordered_groups_indexes], ahc_groupes[ordered_groups_indexes]))
 
 
-
 def PCA_plots():
 
     fromage = pd.read_table(r"./fromage1.txt", sep="\t", header=0, index_col=0)
     pca = PCA(n_components=2).fit_transform(fromage)
-    
 
     # kmeans PCA plot
     np.random.seed(0)
@@ -80,25 +78,108 @@ def PCA_plots():
         kmeans_PCA_plt.scatter(pca[kmeans.labels_ == k, 0],
                                pca[kmeans.labels_ == k, 1], c=couleur)
 
+    # AHC PCA plot
 
-    #AHC PCA plot
-    
     Z = linkage(fromage, method='ward', metric='euclidean')
     AHC_groupes = fcluster(Z, t=255, criterion='distance')
     fig = plt.figure(figsize=(8, 8))
     AHC_PCA_plt = fig.add_subplot(1, 1, 1)
     AHC_PCA_plt.set_title('PCA AHC', fontsize=20)
-    
-    for couleur, k in zip(['red', 'blue', 'lawngreen', 'aqua'], [1,2,3,4]):
-        AHC_PCA_plt.scatter(pca[AHC_groupes== k, 0],
-                               pca[AHC_groupes == k, 1], c=couleur)
 
+    for couleur, k in zip(['red', 'blue', 'lawngreen', 'aqua'], [1, 2, 3, 4]):
+        AHC_PCA_plt.scatter(pca[AHC_groupes == k, 0],
+                            pca[AHC_groupes == k, 1], c=couleur)
 
-    
-    
     plt.show()
 
 
-# PCA_plots()
-agglomerative_hierarchical_clustering()
+def DIANA_based_on_KMeans():
 
+    def split(group):
+        np.random.seed(0)
+        kmeans = cluster.KMeans(n_clusters=2).fit(group)
+        cluster0 = group[kmeans.labels_ == 0]
+        cluster1 = group[kmeans.labels_ == 1]
+        # print(kmeans.cluster_centers_[0])
+        distance = metrics.pairwise.euclidean_distances(
+            [kmeans.cluster_centers_[0]], [kmeans.cluster_centers_[1]])
+        return [cluster0, cluster1, distance[0][0]]
+
+    def adjust_linkage_pointers(linkage, n_samples):
+        adjusted_linkage = []
+        for i, [index0, index1, distance, size] in enumerate(linkage):
+            if(index0 < 0):
+                index0 = n_samples+i+index0
+            if(index1 < 0):
+                index1 = n_samples+i+index1
+            adjusted_linkage += [[float(index0), float(index1),
+                                  float(distance), float(size)]]
+
+        return adjusted_linkage
+
+    fromage = pd.read_table(r"./fromage1.txt", sep="\t", header=0, index_col=0)
+    groups = [fromage]
+
+    linkage = []
+    maxDistance = 60
+    i = 0
+    while(i < len(groups)):
+        currentGroup = groups[i]
+        [cluster0, cluster1,distance] = split(currentGroup)
+
+        if(len(cluster0) == 1):
+            index0 = fromage.index.get_loc(cluster0.index[0])
+            if(len(cluster1) == 1):
+                index1 = fromage.index.get_loc(cluster1.index[0])
+            else:
+                index1 = i-len(groups)
+                groups += [cluster1]
+        else:
+            index0 = i-len(groups)
+            if(len(cluster1) == 1):
+                index1 = fromage.index.get_loc(cluster1.index[0])
+            else:
+                index1 = i-len(groups)-1
+                groups += [cluster1]
+            groups += [cluster0]
+
+        # distance = maxDistance-2*i
+        size = len(currentGroup)
+        linkage.insert(0, [index0, index1, distance, size])
+
+        i += 1
+
+    linkage = adjust_linkage_pointers(linkage, len(fromage))
+    dendrogram(linkage, labels=fromage.index,
+               orientation='left', color_threshold=255)
+
+    plt.show()
+
+
+DIANA_based_on_KMeans()
+
+
+# print( np.argsort(km1.labels_))
+# indexes0=fromage.index[km1.labels_==0]
+# indexes0=fromage.loc[['CarredelEst']]
+# indexes0=fromage.index.get_loc('CarredelEst')
+# print(indexes0)
+# print(metrics.silhouette_samples(fromage, km1.labels_))
+# km2 = cluster.KMeans(n_clusters=2).fit(fromage[km1.labels_ == 0])
+# print( np.argsort(km2.labels_))
+
+# PCA_plots()
+# agglomerative_hierarchical_clustering()
+# Z=[
+#     [0.0, 3.0, 1.0, 2.0],
+#     [1.0, 5.0, 2.0, 3.0],
+#     [2.0, 4.0, 3.0, 2.0],
+#     [6.0, 7.0, 4.0, 4.0]
+# ]
+
+# inverted=[
+#     []
+# ]
+
+# dendrogram(inverted[::0])
+# plt.show()
